63796fc7abf219952e8f994284fed5a9bceb3a17
yg1110 <younggil9488@gmail.com>
2020-09-30 15:26:35 +0900

    Merge <Main /> Page - Main에 CompetitionSection, BannerSwiper, Footer 합침 - Swiper Class명 겹쳐서 에러나던거 수정
    close #49

--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -1,6 +1,37 @@
 import React, { FC } from 'react'
-import Layout from 'Components/Layout'
+import styled from 'Application/Theme'
+import CompetitionSection from 'Compositions/CompetitionSection'
+import { dummyCompetitionList as competitionList } from '__fixtures__/Competition'
+import BannerSwiper from 'Compositions/BannerSwiper'
+import FooterContainer from 'Compositions/Footer'
 
-const Main: FC = () => <Layout>Hello, Programmers-clone!</Layout>
+const MainContainer = styled.main`
+  min-height: calc(100vh - 3.125rem - 21.125rem - 5rem);
+`
 
+const ListCompetittionSection = styled.section`
+  padding: 5rem 0 7rem 0;
+  display: block;
+`
+
+const BannerSwiperSection = styled.section`
+  margin: 5rem 0 0 0;
+  display: block;
+`
+
+const Main: FC = () => (
+  <MainContainer>
+    <ListCompetittionSection>
+      <CompetitionSection
+        headerTitle="채용프로그램"
+        buttonText="더보기"
+        competitionList={competitionList}
+      />
+    </ListCompetittionSection>
+    <BannerSwiperSection>
+      <BannerSwiper />
+    </BannerSwiperSection>
+    <FooterContainer />
+  </MainContainer>
+)
 export default Main

000120bffe69e504f2d231bc83709cffd01a79af
rightpair <tkddn204@gmail.com>
2020-10-12 13:36:17 +0900

    Updates

---
+++ precache-manifest.ae034c3928101232e36de869b7d74f6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe56984062b615e90cfa64249979b17",
+    "url": "/programmers-clone/index.html"
+  },
+  {
+    "revision": "8f55cdfcea4a21b11514",
+    "url": "/programmers-clone/static/js/2.f0e9c04d.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/programmers-clone/static/js/2.f0e9c04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f630fd35b7783ead273",
+    "url": "/programmers-clone/static/js/main.8c25f5e8.chunk.js"
+  },
+  {
+    "revision": "284bc2c70ca6b14f6d48",
+    "url": "/programmers-clone/static/js/runtime-main.b5753014.js"
+  }
+]);
